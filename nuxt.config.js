06048ae3d4760a6f9ecfcee01c66919f720ba0fc
gifarz <gifarz06@gmail.com>
2023-04-14 10:47:19 +0700

    change title in nux.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
   head: {
-    title: 'nuxt-project',
+    title: 'The Moments',
     htmlAttrs: {
       lang: 'en'
     },
